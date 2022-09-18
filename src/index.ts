ebecb5be596954d116b89019054ae452a7aee94e
ittyi <vioiv.k4@gmail.com>
2022-09-18 09:30:19 +0900

    ✨ CLIからの入力を受け取れるように。

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,31 @@ const printLine = (text: string, breakLine: boolean = true) => {
 	return process.stdout.write(text + (breakLine ? `\n`: ''))
 }
 
-
+// printLine test code
 printLine("hello");
 printLine("my name is Ittyi!", false);
 printLine("have nice day~~", false);
+
+
+const promptInput = async (text: string) => {
+	printLine(`\n${text}\n`, false)
+	const input :string = await new Promise(// 非同期処理突入
+		(resolve) => process.stdin.once('data', (data) => resolve(data.toString()))
+	)
+
+	return input.trim();
+}
+
+// promptInput test code
+// ↓この書き方じゃテストできない！
+// const test = promptInput('test:');
+// printLine(String(test));
+
+;(
+	async () => {
+		const name = await promptInput('Please enter your name');
+		console.log(name);
+		const age = await promptInput('Please enter your age');
+		console.log(age);
+	}
+)();
