81d81c06cc8e2c32944bb4deaa424c1084b83728
ittyi <vioiv.k4@gmail.com>
2022-09-19 16:21:52 +0900

    ♻️ typeの追加

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+type Mode = 'normal' | 'hard';
+
 const printLine = (text: string, breakLine: boolean = true) => {
 	return process.stdout.write(text + (breakLine ? `\n`: ''))
 }
@@ -15,9 +17,9 @@ class HitAndBlow {
 	private readonly answerSource: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 	private answer: string[] = [];
 	private tryCount: number = 0;
-	private mode: 'normal' | 'hard';
+	private mode: Mode;
 
-	constructor (mode: 'normal' | 'hard') {
+	constructor (mode: Mode) {
 		this.mode = mode;
 	}
 
@@ -27,6 +29,9 @@ class HitAndBlow {
 				return 3;
 			case 'hard':
 				return 4;
+			default :
+				const neverVal: never = this.mode;
+				throw new Error(`${neverVal} は無効なモードです。`)
 		}
 	}
 
