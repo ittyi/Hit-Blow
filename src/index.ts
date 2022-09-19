e9311dec7cbb907d8dcf863c4fdbbd541d970b69
ittyi <vioiv.k4@gmail.com>
2022-09-19 22:00:53 +0900

    🩹 わかりやすいように、汎用的な機能は上に

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,39 @@
 /* common feature */
+const printLine = (text: string, breakLine: boolean = true) => {
+	return process.stdout.write(text + (breakLine ? `\n`: ''))
+}
+
+const promptInput = async (text: string) => {
+	printLine(`\n${text}\n`, false);
+	return readLine();
+}
+
+const readLine = async () => {
+	const input: string = await new Promise(
+		(resolve) => process.stdin.once(
+			'data',
+			(data) => resolve(data.toString()) 
+		)
+	)
+	return input.trim()
+}
+
+const promptSelect = async <T extends string>(text: string, values: readonly T[]): Promise<T> => {
+	printLine(`\n${text}`);
+	values.forEach((value) => {
+		printLine(`- ${value}`);
+	})
+
+	printLine(`> `, false);
+
+	const input = await readLine() as T;
+	if (values.includes(input)) {
+		return input;
+	} else {
+		return promptSelect<T>(text, values);
+	}
+}
+
 const nextActions = ['play again', 'exit'] as const;
 type NextAcion = typeof nextActions[number];
 
@@ -38,42 +73,6 @@ class GameProcedure {
 /* hit&blow */
 const modes = ['normal', 'hard'] as const;
 type Mode = typeof modes[number];
-
-const printLine = (text: string, breakLine: boolean = true) => {
-	return process.stdout.write(text + (breakLine ? `\n`: ''))
-}
-
-const promptInput = async (text: string) => {
-	printLine(`\n${text}\n`, false);
-	return readLine();
-}
-
-const readLine = async () => {
-	const input: string = await new Promise(
-		(resolve) => process.stdin.once(
-			'data',
-			(data) => resolve(data.toString()) 
-		)
-	)
-	return input.trim()
-}
-
-const promptSelect = async <T extends string>(text: string, values: readonly T[]): Promise<T> => {
-	printLine(`\n${text}`);
-	values.forEach((value) => {
-		printLine(`- ${value}`);
-	})
-
-	printLine(`> `, false);
-
-	const input = await readLine() as T;
-	if (values.includes(input)) {
-		return input;
-	} else {
-		return promptSelect<T>(text, values);
-	}
-}
-
 
 /* main class */
 class HitAndBlow {
